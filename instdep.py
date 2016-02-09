a6c5058f94e5f3f9962bd3156b7d7d7bd19bd578
Ismo Torvinen <ismo.torvinen@gmail.com>
2016-02-09 22:14:45 +0200

    added working dir option. using '@'

--- instdep.py
+++ instdep.py
@@ -9,12 +9,16 @@ class client:
 		self.program_name = program_name
 		self.repositories = []
 		self.args = []
+		self.working_dir = ''
 
 	def add_arg (self, arg):
 		self.args.append(arg)
 
 	def add_repo (self, repo):
 		self.repositories.append(repo)
+
+	def set_working_dir(self,dir):
+		self.working_dir = dir
 
 	def verbose (self):
 		print "list of commands:"
@@ -23,9 +27,16 @@ class client:
 
 	def execute(self):
 		for repo in self.repositories:
-			print [self.program_name] + self.args + [repo]			
-			out = subprocess.check_output([self.program_name] + self.args + [repo])
+			if self.working_dir == '':
+				print [self.program_name] + self.args + [repo]			
+				out = subprocess.check_output([self.program_name] + self.args + [repo])
+			else:
+				print [self.program_name] + self.args + [repo], "in", self.working_dir
+				out = subprocess.check_output([self.program_name] + self.args + [repo], cwd=self.working_dir)
+
 			print out
+
+
 if __name__ == "__main__":
 	lines = []
 	clients = []
@@ -43,6 +54,8 @@ if __name__ == "__main__":
 					first = False
 				elif stripped[0] == '+':
 					current_client.add_arg(stripped[1:])
+				elif stripped[0] == '@':
+					current_client.set_working_dir(stripped[1:])
 				else:
 					current_client.add_repo(stripped)
 	clients.append(current_client)
