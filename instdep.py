c191c6076f5991a6c20c813b6073345154db57d2
Ismo Torvinen <ismo.torvinen@gmail.com>
2016-02-09 20:09:21 +0200

    first version

---
+++ instdep.py
@@ -0,0 +1,55 @@
+import fileinput
+import subprocess
+
+class client:
+
+
+	def __init__(self, program_name): 
+		self.program_name = program_name
+		self.repositories = []
+		self.args = []
+
+	def add_arg (self, arg):
+		self.args.append(arg)
+
+	def add_repo (self, repo):
+		self.repositories.append(repo)
+
+	def verbose (self):
+		print "list of commands:"
+		for repo in self.repositories:
+			print [self.program_name] + self.args + [repo]
+
+	def execute(self):
+		for repo in self.repositories:
+			print [self.program_name] + self.args + [repo]			
+			out = subprocess.check_output([self.program_name] + self.args + [repo])
+			print out
+lines = []
+clients = []
+first = True
+
+for line in fileinput.input():
+
+	stripped = line.strip()
+	if stripped:
+		if not stripped[0] == '#':
+			if stripped[0] == '=':
+				if first == False:
+					clients.append(current_client)
+				current_client = client(stripped[1:])
+				first = False
+			elif stripped[0] == '+':
+				current_client.add_arg(stripped[1:])
+			else:
+				current_client.add_repo(stripped)
+clients.append(current_client)
+
+for cli in clients:
+	cli.execute()
+	
+
+#with open('dependencies.txt', 'r') as infile:
+#	data = infile.read()
+#lines = data.splitlines()
+
